0054b101096041e26e6bc295fbb7aab3dd2503f0
Zhuchara32 <vovazhuchara@gmail.com>
2021-12-09 18:28:06 +0200

    deploy

---
+++ precache-manifest.76f81f0a2af033d177bb15a6cd8ab82b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deea6ba22bbc7c03fb9f",
+    "url": "/filancy/css/app.c449a1fa.css"
+  },
+  {
+    "revision": "c5a882d6c87842684023a9c4d94aba08",
+    "url": "/filancy/img/image.c5a882d6.png"
+  },
+  {
+    "revision": "fe88f0502d9b45314f8bbf0e003b8740",
+    "url": "/filancy/img/loon-icon.fe88f050.png"
+  },
+  {
+    "revision": "582cffc1c27b50a9fe3d3d56d97ebae1",
+    "url": "/filancy/index.html"
+  },
+  {
+    "revision": "deea6ba22bbc7c03fb9f",
+    "url": "/filancy/js/app.1c788563.js"
+  },
+  {
+    "revision": "ce9095139e7fd4c152d5",
+    "url": "/filancy/js/chunk-vendors.578b9635.js"
+  },
+  {
+    "revision": "21fff9f0d893261f529be4e80b7e662a",
+    "url": "/filancy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/filancy/robots.txt"
+  }
+]);
